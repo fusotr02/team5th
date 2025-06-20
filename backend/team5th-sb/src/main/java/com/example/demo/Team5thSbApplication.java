875f9f1834e0e585bbf70caf039ee041bdc932c2
koga <koga@ia.com>
2025-06-20 11:39:11 +0900

    backend

---
+++ backend/team5th-sb/src/main/java/com/example/demo/Team5thSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5thSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5thSbApplication.class, args);
+	}
+
+}
