aa1559a5b4d30807720d4710cdc01b41b4784515
Higuchi <Higuchi@nmt/com>
2025-06-24 10:13:52 +0900

    bookmark.javaの内容追加

--- backend/team5th-sb/src/main/java/com/example/demo/travelvooking/model/Bookmark.java
+++ backend/team5th-sb/src/main/java/com/example/demo/travelvooking/model/Bookmark.java
@@ -1,5 +1,34 @@
 package com.example.demo.travelvooking.model;
 
-public class Bookmark {
+	import java.time.LocalDateTime;
+
+	import jakarta.persistence.Column;
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.GeneratedValue;
+	import jakarta.persistence.GenerationType;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.JoinColumn;
+	import jakarta.persistence.ManyToOne;
+	import jakarta.persistence.Table;
+
+	@Entity
+	@Table(name = "bookmarks")
+	public class Bookmark {
+		 @Id
+		    @GeneratedValue(strategy = GenerationType.IDENTITY)
+		    @Column(name = "bookmark_id")
+		    private Integer bookmarkId;
+		 
+		 @ManyToOne
+		    @JoinColumn(name = "user_id", nullable = false)
+		    private User user; // 外部キー：ユーザー
+		 
+		 @ManyToOne
+		    @JoinColumn(name = "hotel_id", nullable = false)
+		    private Hotel hotel; // 外部キー：ホテル
+		 
+		 @Column(name = "registered_at", nullable = false)
+		 private LocalDateTime registeredAt;// 登録日：現在時刻がデフォルト
+	}
 
 }
