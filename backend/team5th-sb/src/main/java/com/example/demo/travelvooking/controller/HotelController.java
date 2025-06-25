61060e16751b7bd2db791a6513d00db0028c5cff
kinoshita <kinoshita@ia.com>
2025-06-25 10:41:00 +0900

    ホテルコントローラー

--- backend/team5th-sb/src/main/java/com/example/demo/travelvooking/controller/HotelController.java
+++ backend/team5th-sb/src/main/java/com/example/demo/travelvooking/controller/HotelController.java
@@ -1,5 +1,31 @@
 package com.example.demo.travelvooking.controller;
 
-public class HotelController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.travelvooking.dto.HotelResponse;
+import com.example.demo.travelvooking.service.HotelService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/hotels")
+
+public class HotelController {
+	 private final HotelService hotelService;
+
+	    @GetMapping
+	    public List<HotelResponse> getAllHotels() {
+	        return hotelService.getAllHotels();
+	    }
+
+	    @GetMapping("/{id}")
+	    public HotelResponse getHotelById(@PathVariable Long id) {
+	        return hotelService.getHotelByID(id);
+	    }
 }
