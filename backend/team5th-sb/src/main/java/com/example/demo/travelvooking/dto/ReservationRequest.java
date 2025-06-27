1e7e6ad56a8e89e693f4743917496f1b99e1d606
koga <koga@ia.com>
2025-06-27 11:23:05 +0900

    application.property

--- backend/team5th-sb/src/main/java/com/example/demo/travelvooking/dto/ReservationRequest.java
+++ backend/team5th-sb/src/main/java/com/example/demo/travelvooking/dto/ReservationRequest.java
@@ -1,77 +1,16 @@
 package com.example.demo.travelvooking.dto;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
-import com.example.demo.travelvooking.model.Hotel;
-import com.example.demo.travelvooking.model.User;
+import lombok.Data;
 
+@Data
 public class ReservationRequest {
-	private Long id;
-    private User user;
-    private Hotel hotel;
+	private Long userId; //ユーザーID
+    private Long hotelId;
     private Date checkin_date;
     private Date checkout_date;
-    private int people;
-    private int rooms;
-    private String status;
-    private LocalDateTime reservation_date;
-    
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public User getUser_id() {
-		return user;
-	}
-	public void setUser_id(User user_id) {
-		this.user = user_id;
-	}
-	public Hotel getHotel_id() {
-		return hotel;
-	}
-	public void setHotel_id(Hotel hotel_id) {
-		this.hotel = hotel_id;
-	}
-	public Date getCheckin_date() {
-		return checkin_date;
-	}
-	public void setCheckin_date(Date checkin_date) {
-		this.checkin_date = checkin_date;
-	}
-	public Date getCheckout_date() {
-		return checkout_date;
-	}
-	public void setCheckout_date(Date checkout_date) {
-		this.checkout_date = checkout_date;
-	}
-	public int getPeople() {
-		return people;
-	}
-	public void setPeople(int people) {
-		this.people = people;
-	}
-	public int getRooms() {
-		return rooms;
-	}
-	public void setRooms(int rooms) {
-		this.rooms = rooms;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public LocalDateTime getReservation_date() {
-		return reservation_date;
-	}
-	public void setReservation_date(LocalDateTime reservation_date) {
-		this.reservation_date = reservation_date;
-	}
-	
-	
+	private Integer people;
+	private Integer rooms;
 
 }
